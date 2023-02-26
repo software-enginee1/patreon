9af394773740fac447bf84f84822a54d8f4c8baa
stellardropthebeat <nutchapol1111@gmail.com>
2023-02-26 16:10:09 +0700

    First setup

---
+++ src/store.js
@@ -0,0 +1,67 @@
+import { createStore } from 'vuex'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
+
+import { auth } from "src/firebase";
+
+const store = createStore({
+    state: {
+        user: {
+            loggedIn: false,
+            data: null
+        }
+    },
+    getters: {
+        user(state){
+            return state.user
+        }
+    },
+    mutations: {
+        SET_LOGGED_IN(state, value) {
+            state.user.loggedIn = value;
+        },
+        SET_USER(state, data) {
+            state.user.data = data;
+        }
+    },
+    actions: {
+        async register(context, { email, password, name}){
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            if (response) {
+                context.commit('SET_USER', response.user)
+                await response.user.updateProfile({displayName: name})
+            } else {
+                throw new Error('Unable to register user')
+            }
+        },
+
+        async logIn(context, { email, password }){
+            const response = await signInWithEmailAndPassword(auth, email, password)
+            if (response) {
+                context.commit('SET_USER', response.user)
+            } else {
+                throw new Error('login failed')
+            }
+        },
+
+        async logOut(context){
+            await signOut(auth)
+            context.commit('SET_USER', null)
+        },
+
+        async fetchUser(context ,user) {
+            context.commit("SET_LOGGED_IN", user !== null);
+            if (user) {
+                context.commit("SET_USER", {
+                    displayName: user.displayName,
+                    email: user.email
+                });
+            } else {
+                context.commit("SET_USER", null);
+            }
+        }
+    }
+})
+
+
+export default store
+
